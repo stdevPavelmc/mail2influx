64673c9261a88efa52ef6a0981886f9a9f9a19e9
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2020-10-21 13:11:35 -0400

    Add deferred_TLS_error measurement

--- mail2influx.py
+++ mail2influx.py
@@ -159,10 +159,9 @@ def filter_postfix(line):
 		['postfix_deferred_connection_refused',		"^.* postfix.*: .* status=deferred .*Connection refused.*$"],
 		['postfix_deferred_greylisting',			"^.* postfix.*: .* status=deferred .*Greylisting enabled.*$"],
 		['postfix_deferred_too_many_recipients',	"^.* postfix.*: .* status=deferred .*Too many recipients received from the sender.*$"],
-		['postfix_deferred_server_error',			"^.* postfix.*: .* status=deferred .*Client host rejected.* Server configuration error.*$"],
+		['postfix_deferred_server_error',	"^.* postfix.*: .* status=deferred .*Client host rejected.* Server configuration error.*$"],
+		['postfix_deferred_TLS_error',		"^.* postfix.*: .* status=deferred .*Cannot start TLS.*$"],
 		
-
-
 	]
 
 	#### pure data messages
